fdb51787f2acf7c94352a286ca9b4bd7f33fe2ce
Alexander Skelton <ajrskelton@gmail.com>
2022-05-11 22:25:47 +0100

    Add info cards as children

--- src/Components/QuestionFrame.tsx
+++ src/Components/QuestionFrame.tsx
@@ -8,8 +8,6 @@ function QuestionFrame() {
     const activeIndex = useSelector((state: RootState) => state.question.activeIndex);
     const selectedOptions: any = useSelector((state: RootState) => state.select.selectedOptions);
     const dispatch = useDispatch();
-
-    const maxCorrectnessLevel = 4;
     
     let isFirstQuestion = activeIndex === 0;
     let previousQuestionMarkup = (
@@ -40,34 +38,29 @@ function QuestionFrame() {
     }
 
     const activeQuestion = questionData[activeIndex];
-    const rowsWithResponses = Object.keys(selectedOptions);
-
-    let correctAnswers = 0;
-    let totalAnswers = activeQuestion.answer.rows.length;
     
-    for (let answerRow of activeQuestion.answer.rows) {
-        if (rowsWithResponses.includes(answerRow.id)) {
-            let responseId = selectedOptions[answerRow.id];
-            let isCorrect = answerRow.options.find(x => x.id === responseId)?.isCorrect;
-            if (isCorrect) {
-                correctAnswers++;
-            }
-        }
-    }
-    let isLocked = correctAnswers === totalAnswers;
-    let correctnessLevel = Math.round(correctAnswers / totalAnswers * maxCorrectnessLevel);
-    let pageClass = `page correct-${correctnessLevel}`;
-
     return (
-        <div className={pageClass}>
-            <div className="frame">
-                <div className="display-panel flex-column">
-                    <QuestionView question={activeQuestion} isLocked={isLocked} />
-                </div>
-                <div className="navigation-panel flex-row">
-                    {previousQuestionMarkup}
-                    {nextQuestionMarkup}
-                </div>
+        <div className="frame">
+            <div className="display-panel flex-column">
+                <QuestionView question={questionData[0]} isVisible={0 === activeIndex}>
+                    <img className='small-image' src='water.jpg' alt='water' />
+                        Water (chemical formula H2O) is an inorganic, transparent, tasteless, odorless, and nearly colorless chemical substance,
+                        which is the main constituent of Earth's hydrosphere and the fluids of all known living organisms (in which it acts as a solvent).
+                        It is vital for all known forms of life, even though it provides neither food, energy, nor organic micronutrients.
+                        Its chemical formula, H2O, indicates that each of its molecules contains one oxygen and two hydrogen atoms, connected by covalent bonds.
+                        The hydrogen atoms are attached to the oxygen atom at an angle of 104.45°. 
+                        Water" is the name of the liquid state of H2O at standard conditions for temperature and pressure. 
+                </QuestionView>
+                <QuestionView question={questionData[1]} isVisible={1 === activeIndex}>
+                    <img className='large-image' src='fire.jpg' alt='fire' />
+                </QuestionView>
+                <QuestionView question={questionData[2]} isVisible={2 === activeIndex}>
+                    <img className='large-image' src='fish.jpg' alt='fish' />
+                </QuestionView>
+            </div>
+            <div className="navigation-panel flex-row">
+                {previousQuestionMarkup}
+                {nextQuestionMarkup}
             </div>
         </div>
     );
--- src/Components/QuestionView.tsx
+++ src/Components/QuestionView.tsx
@@ -1,11 +1,60 @@
+import { useSelector } from 'react-redux';
+import { RootState } from '../app/store';
 import { Question } from '../Types/Question';
 import AnswerView from './AnswerView';
 
-function QuestionView(props: { question: Question, isLocked: boolean }) {
+function QuestionView(props: { children: React.ReactNode, question: Question, isVisible: boolean }) {
+    
+    const selectedOptions: any = useSelector((state: RootState) => state.select.selectedOptions);
+
+    if (!props.isVisible) {
+        return null;
+    }
+
+    const maxCorrectnessLevel = 4;
+
+    const getCorrectAnswers = (question: Question, selectedOptions: any) => {
+        let correctAnswers = 0;
+        const rowsWithResponses = Object.keys(selectedOptions);
+        for (let answerRow of question.answer.rows) {
+            if (rowsWithResponses.includes(answerRow.id)) {
+                let responseId = selectedOptions[answerRow.id];
+                let isCorrect = answerRow.options.find(x => x.id === responseId)?.isCorrect;
+                if (isCorrect) {
+                    correctAnswers++;
+                }
+            }
+        }
+        return correctAnswers;
+    };
+
+    const checkLocked = (question: Question, selectedOptions: any) => {
+        let totalAnswers = question.answer.rows.length;
+        return (totalAnswers === getCorrectAnswers(question, selectedOptions));
+    };
+
+    const getCardClass = (question: Question, selectedOptions: any) => {
+        let totalAnswers = question.answer.rows.length;
+        let correctAnswers = getCorrectAnswers(question, selectedOptions);
+        let correctnessLevel = Math.round(correctAnswers / totalAnswers * maxCorrectnessLevel);
+        return `card correct-${correctnessLevel}`;
+    };
+    
+    let isLocked = checkLocked(props.question, selectedOptions);
+    let cardClass = getCardClass(props.question, selectedOptions);
+    let infoCard = (
+        <div className='card'>
+            { props.children }
+        </div>
+    );
+    
     return (
         <>
-            <h2>{props.question.questionText}</h2>
-            <AnswerView answer={props.question.answer} isLocked={props.isLocked} />
+            <div className={cardClass}>
+                <h2>{props.question.questionText}</h2>
+                <AnswerView answer={props.question.answer} isLocked={isLocked} />
+            </div>
+            { isLocked ? infoCard : null }
         </>
     );
 }
