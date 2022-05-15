6f542525a01927cebc32ee9ae6cfc297ba68dff2
Alexander Skelton <ajrskelton@gmail.com>
2022-05-15 19:11:57 +0100

    Replace background colour highlight with animated slider

--- src/Components/AnswerRowView.tsx
+++ src/Components/AnswerRowView.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { Row } from '../Types/Row';
 import { ToggleOption } from '../Types/ToggleOption';
 import { shuffle } from '../util/helpers';
@@ -9,13 +9,54 @@ import { RootState } from '../app/store';
 
 function AnswerRowView(props: { row: Row, isLocked: boolean }) {
 
+    const rowRef = useRef<HTMLDivElement>(null);
+    const [windowSize, setWindowSize] = useState({
+        width: 0,
+        height: 0,
+    });
     const activeResponses: any = useSelector((state: RootState) => state.select.activeResponses);
+
+    const [rect, setRect] = useState({
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        height: 0,
+        width: 0
+    });
+
+    
+    useEffect(() => {
+        function handleResize() {
+            setWindowSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        }
+        window.addEventListener(`resize-${props.row.id}`, handleResize);
+        handleResize();
+        return () => window.removeEventListener(`resize-${props.row.id}`, handleResize);
+    }, [windowSize]);
+
+    const measuredRef = useCallback((node: any) => {
+        if (node !== null) {
+            let clientRect = node.getBoundingClientRect()
+            setRect({
+                top: Math.round(clientRect.top),
+                right: Math.round(clientRect.right),
+                bottom: Math.round(clientRect.bottom),
+                left: Math.round(clientRect.left),
+                width: Math.round(clientRect.width),
+                height: Math.round(clientRect.height)
+            });
+        }
+    }, [windowSize]);
 
     let sizePercentage = 0;
     if (props.row.options.length === 3) {
-        sizePercentage = 33;
+        sizePercentage = 0.33333;
     } else {
-        sizePercentage = 50;
+        sizePercentage = 0.50;
     }
 
     let index = 0;
@@ -27,17 +68,19 @@ function AnswerRowView(props: { row: Row, isLocked: boolean }) {
     }
     let highlightCss: CSS.Properties = {
         position: 'absolute',
-        top: '0',
-        left: '0',
-        background: 'rgba(255,255,255,0.8)',
-        width: `${sizePercentage}%`,
-        transform: `translate(${index*sizePercentage}%)`
+        top: `${rect.top}px`,
+        left: `${rect.left}px`,
+        height: `${rect.height}px`,
+        background: 'rgba(255,255,255,0.4)',
+        width: `${rect.width*sizePercentage}px`,
+        transform: `translate(${index*rect.width*sizePercentage}px)`,
+        borderRadius: '100px'
     };
 
     return (
         <>
-            <div className="flex-row row">
-                <div style={highlightCss}>&nbsp;</div>
+            <div className="flex-row row" ref={measuredRef}>
+                <div className="highlight" style={highlightCss}>&nbsp;</div>
                 {props.row.options.map((option, i) => {
                     return (<ToggleOptionView key={option.id} row={props.row} option={option} isLocked={props.isLocked} />) 
                 })}
---
+++ src/Types/Rectangle.ts
@@ -0,0 +1,6 @@
+export interface Rectangle {
+    top: number,
+    right: number,
+    bottom: number,
+    left: number
+}
